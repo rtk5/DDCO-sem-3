354a54e971043c270654a2bbe712cc367d6cf928
rtk5 <rithvik.matta@gmail.com>
2024-08-31 13:53:34 +0530

    aug 31

--- week 4/3) 1:2 demux/demux_tb.v
+++ week 4/3) 1:2 demux/demux_tb.v
@@ -7,7 +7,7 @@ wire y1;
 demux uut (.i(i),.sel(sel),.y0(y0),.y1(y1));
 
 initial begin
-#5; i = 1'b0; sel = 1'b0;
+#0; i = 1'b0; sel = 1'b0;
 #5; i = 1'b0; sel = 1'b1;
 #5; i = 1'b1; sel = 1'b1;
 end
